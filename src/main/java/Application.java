ea00d942cb223416015d2294767efe9fa3dd458a
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-11-19 17:45:34 -0300

    Adds option to run on Application main.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,19 @@
+import component.Receiver;
+import component.Sender;
+import helper.FileOperations;
+
 public class Application {
 
     public static void main(String[] args) {
         try{
-            System.out.println("Nothing here...");
+            // Program args: receiver
+            if(args[0].equals("receiver")){
+                new Receiver();
+            }
+            // Program args: file/path/ sender
+            else if(args[1].equals("sender")){
+                new Sender(FileOperations.readFileAndReturnBytePartsAsPackets(args[0]));
+            }
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
